09fb2dbb363bc67398833449eb2ec154146bf960
RMhoon <r_mhoon@hotmail.com>
2021-03-16 20:50:31 -0400

    Save to repo

---
+++ BasicClass_Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClass_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "Mercedes";
+            car.Model = "350";
+            car.Year = 2020;
+
+            Console.WriteLine($"The car is for sale, Make({car.Make}), Model ({car.Model}) and Year({car.Year})");
+        }
+    }
+}
